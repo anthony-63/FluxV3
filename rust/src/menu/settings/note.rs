10c0bc24ee24b4c78c6a229bfe690ad034eccb51
anthony-63 <back.up.email.158@gmail.com>
2024-05-26 19:27:50 -0400

    add fadein setting

--- rust/src/menu/settings/note.rs
+++ rust/src/menu/settings/note.rs
@@ -10,6 +10,7 @@ pub struct NoteSettings {
     approach_rate: Option<Gd<SpinBox>>,
     approach_time: Option<Gd<SpinBox>>,
     approach_distance: Option<Gd<SpinBox>>,
+    fade_in: Option<Gd<SpinBox>>,
     pushback: Option<Gd<CheckButton>>,
 }
 
@@ -22,6 +23,7 @@ impl IControl for NoteSettings {
             approach_distance: None,
             approach_rate: None,
             approach_time: None,
+            fade_in: None,
             pushback: None,
         }
     }
@@ -31,18 +33,21 @@ impl IControl for NoteSettings {
         let mut approach_rate = self.base().get_node_as::<SpinBox>("GridContainer/Approach/Rate");
         let mut approach_distance = self.base().get_node_as::<SpinBox>("GridContainer/Approach/Distance");
         let mut approach_time = self.base().get_node_as::<SpinBox>("GridContainer/Approach/Time");
+        let mut fade_in = self.base().get_node_as::<SpinBox>("GridContainer/Approach/FadeIn");
         let mut pushback = self.base().get_node_as::<CheckButton>("GridContainer/Approach/Pushback");
         
         approach_type.connect("item_selected".into(), self.base_mut().callable("update_type"));
         approach_distance.connect("value_changed".into(), self.base_mut().callable("update_ad"));
         approach_rate.connect("value_changed".into(), self.base_mut().callable("update_ar"));
         approach_time.connect("value_changed".into(), self.base_mut().callable("update_at"));
+        fade_in.connect("value_changed".into(), self.base_mut().callable("update_fade_in"));
         pushback.connect("toggled".into(), self.base_mut().callable("update_pushback"));
 
         self.approach_type = Some(approach_type.clone());
         self.approach_rate = Some(approach_rate.clone());
         self.approach_distance = Some(approach_distance.clone());
         self.approach_time = Some(approach_time.clone());
+        self.fade_in = Some(fade_in.clone());
         self.pushback = Some(pushback.clone());
 
         self.update_settings();
@@ -58,6 +63,7 @@ impl NoteSettings {
         self.approach_rate.as_mut().unwrap().set_value(settings.note.approach_rate as f64);
         self.approach_distance.as_mut().unwrap().set_value(settings.note.approach_distance as f64);
         self.approach_time.as_mut().unwrap().set_value(settings.note.approach_time as f64);
+        self.fade_in.as_mut().unwrap().set_value(settings.note.fade_in as f64);
         self.pushback.as_mut().unwrap().set_pressed(settings.note.pushback);
 
         match settings.note.approach_mode  {
@@ -117,6 +123,15 @@ impl NoteSettings {
     fn update_at(&mut self, value: f64) {
         unsafe { 
             FLUX.settings.as_mut().unwrap().note.approach_time = value as f32;
+            FLUX.settings.as_mut().unwrap().update(false);
+        }
+        self.update_settings();
+    }
+
+    #[func]
+    fn update_fade_in(&mut self, value: f64) {
+        unsafe { 
+            FLUX.settings.as_mut().unwrap().note.fade_in = value as f32;
             FLUX.settings.as_mut().unwrap().update(false);
         }
         self.update_settings();
