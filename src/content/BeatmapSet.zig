000978de2d6a23629157af838b34b11014165367
anthony-63 <back.up.email.158@gmail.com>
2024-06-10 21:47:47 -0400

    beatmapset parsing

--- src/content/BeatmapSet.zig
+++ src/content/BeatmapSet.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const Beatmap = @import("Beatmap.zig");
 
 Broken: bool,
@@ -12,3 +13,29 @@ Difficulties: []Beatmap,
 Mappers: [][]const u8,
 MusicPath: []const u8,
 Cover: []const u8,
+
+pub fn loadFromFolder(folder_path: []const u8, allocator: std.mem.Allocator) !void {
+    const meta_path = try std.fmt.allocPrint(allocator, "{s}/meta.json", .{folder_path});
+    defer allocator.free(meta_path);
+
+    const meta_file = try std.fs.openFileAbsolute(meta_path, .{});
+    defer meta_file.close();
+
+    const meta_stat = try meta_file.stat();
+
+    const meta_buf = try meta_file.readToEndAlloc(allocator, meta_stat.size);
+    defer allocator.free(meta_buf);
+
+    const MetaFormat = struct {
+        _version: u8,
+        _title: []const u8,
+        _mappers: [][]const u8,
+        _music: []const u8,
+        _difficulties: [][]const u8,
+    };
+
+    const meta = try std.json.parseFromSlice(MetaFormat, allocator, meta_buf, .{});
+    defer meta.deinit();
+
+    std.debug.print("{s}", .{meta.value._title});
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const BeatmapSet = @import("content/BeatmapSet.zig");
 
 pub fn main() !void {
-    std.debug.print("flux", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        _ = gpa.deinit();
+    }
+
+    BeatmapSet.loadFromFolder("C:/Users/antho/AppData/Roaming/Flux/maps/zitronitro_7_7_-_mii_munbe_plaza", allocator) catch |err| {
+        std.log.err("Error loading beatmap: {any}", .{err});
+    };
 }
