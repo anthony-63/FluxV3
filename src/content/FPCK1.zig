014aeae80593ac32c705dfffa733f2befc25b9fe
anthony-63 <back.up.email.158@gmail.com>
2024-06-12 20:35:19 -0400

    fix fpck1 format

--- src/content/FPCK1.zig
+++ src/content/FPCK1.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
 // format spec
-// section seperated by 66 70 63 6B 31 01 03 01 (fpck1)
+
+// header: first section len(u64) second section len(u64)
 
 // first section: store tree
 // format: id(u32) path_len(u32) path
@@ -11,8 +12,6 @@ const std = @import("std");
 
 // compressed with flate/deflate
 
-const L0SEP = [8]u8{ 0x66, 0x70, 0x63, 0x31, 0x01, 0x03, 0x01, 0xf3 };
-
 fn createFpck1Tree(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList(u8) {
     var data = std.ArrayList(u8).init(allocator);
 
@@ -21,7 +20,7 @@ fn createFpck1Tree(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList
     var walker = try dir.walk(allocator);
     defer walker.deinit();
 
-    std.debug.print("[FPCK1] storing file tree\n", .{});
+    std.debug.print("[FPCK1] creating tree\n", .{});
     while (try walker.next()) |entry| {
         if (entry.kind != .file) {
             continue;
@@ -29,11 +28,11 @@ fn createFpck1Tree(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList
 
         std.debug.print("[FPCK1 STG1] ({any}) file: {any} {any} {s}\n", .{ data.items.len, id, entry.path.len, entry.path });
 
-        var id_buffer: [4]u8 = [4]u8{ 0, 0, 0, 0 };
+        var id_buffer = [_]u8{0} ** 4;
         std.mem.writeInt(u32, &id_buffer, id, .little);
         try data.appendSlice(&id_buffer);
 
-        var path_size_buffer: [4]u8 = [4]u8{ 0, 0, 0, 0 };
+        var path_size_buffer = [_]u8{0} ** 4;
         std.mem.writeInt(u32, &path_size_buffer, @intCast(entry.path.len), .little);
         try data.appendSlice(&path_size_buffer);
 
@@ -42,12 +41,18 @@ fn createFpck1Tree(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList
         id += 1;
     }
 
-    try data.appendSlice(&L0SEP);
+    return data;
+}
 
-    id = 0;
+fn createFpck1Data(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList(u8) {
+    var data = std.ArrayList(u8).init(allocator);
 
-    walker.deinit();
-    walker = try dir.walk(allocator);
+    var id: u32 = 0;
+
+    var walker = try dir.walk(allocator);
+    defer walker.deinit();
+
+    std.debug.print("[FPCK2] creating data section\n", .{});
 
     while (try walker.next()) |entry| {
         if (entry.kind != .file) {
@@ -59,7 +64,7 @@ fn createFpck1Tree(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList
 
         std.debug.print("[FPCK1 STG2] ({any}) file: {any} {any} \n", .{ data.items.len, id, file_stat.size });
 
-        var id_buffer: [4]u8 = [4]u8{ 0, 0, 0, 0 };
+        var id_buffer = [_]u8{0} ** 4;
         std.mem.writeInt(u32, &id_buffer, id, .little);
         try data.appendSlice(&id_buffer);
 
@@ -68,7 +73,7 @@ fn createFpck1Tree(dir: std.fs.Dir, allocator: std.mem.Allocator) !std.ArrayList
 
         _ = try file.readAll(buffer);
 
-        var data_len_buffer: [8]u8 = [8]u8{ 0, 0, 0, 0, 0, 0, 0, 0 };
+        var data_len_buffer = [_]u8{0} ** 8;
         std.mem.writeInt(u64, &data_len_buffer, buffer.len, .little);
         try data.appendSlice(&data_len_buffer);
 
@@ -84,35 +89,73 @@ pub fn pack(to_pack: std.fs.Dir, output: std.fs.File, allocator: std.mem.Allocat
     const tree = createFpck1Tree(to_pack, allocator) catch {
         return error.FailedToCreateTree;
     };
+    defer tree.deinit();
 
-    std.debug.print("total archive size before compression: {d}kb\n", .{@as(f64, @floatFromInt(tree.items.len)) / 1000.0});
-    var stream = std.io.fixedBufferStream(tree.items);
+    const data = createFpck1Data(to_pack, allocator) catch {
+        return error.FailedToCreateData;
+    };
+    defer data.deinit();
+
+    var full_file = std.ArrayList(u8).init(allocator);
+    defer full_file.deinit();
+
+    var section_size_buffer = [_]u8{0} ** 8;
+
+    std.mem.writeInt(u64, &section_size_buffer, tree.items.len, .little);
+    try full_file.appendSlice(&section_size_buffer);
+
+    std.mem.writeInt(u64, &section_size_buffer, data.items.len, .little);
+    try full_file.appendSlice(&section_size_buffer);
+
+    try full_file.appendSlice(tree.items);
+    try full_file.appendSlice(data.items);
+
+    std.debug.print("total archive size before compression: {d}kb\n", .{@as(f64, @floatFromInt(full_file.items.len)) / 1000.0});
+
+    var stream = std.io.fixedBufferStream(full_file.items);
 
     std.compress.flate.compress(stream.reader(), output.writer(), .{ .level = .level_9 }) catch {
         return error.FailedToCompressFlate;
     };
-
-    tree.deinit();
 }
 
 pub fn unpack(file: std.fs.File, output: std.fs.Dir, allocator: std.mem.Allocator) !void {
     var tmp = try std.fs.cwd().createFile("~fpck1.unpacker", .{});
+    defer std.fs.cwd().deleteFile("~fpck1.unpacker") catch {};
 
     try std.compress.flate.decompress(file.reader(), tmp.writer());
     tmp.close();
 
     tmp = try std.fs.cwd().openFile("~fpck1.unpacker", .{});
     const tmp_stat = try tmp.stat();
-    const data = try tmp.readToEndAlloc(allocator, tmp_stat.size);
-    tmp.close();
-    defer std.fs.cwd().deleteFile("~fpck1.unpacker") catch {};
 
+    const data = try tmp.readToEndAlloc(allocator, tmp_stat.size);
     defer allocator.free(data);
 
-    var sections_iter = std.mem.split(u8, data, &L0SEP);
+    tmp.close();
 
-    const tree = sections_iter.next().?;
-    const files = sections_iter.next().?;
+    var data_stream = std.io.fixedBufferStream(data);
+
+    std.debug.print("[FPCK1] getting header file tree\n", .{});
+
+    var tree_size_buffer = [_]u8{0} ** 8;
+    _ = try data_stream.read(&tree_size_buffer);
+    const tree_size = std.mem.readInt(u64, &tree_size_buffer, .little);
+
+    var file_data_size_buffer = [_]u8{0} ** 8;
+    _ = try data_stream.read(&file_data_size_buffer);
+    const file_data_size = std.mem.readInt(u64, &file_data_size_buffer, .little);
+
+    const tree = try allocator.alloc(u8, tree_size);
+    defer allocator.free(tree);
+    _ = try data_stream.read(tree);
+
+    const files = try allocator.alloc(u8, file_data_size);
+    defer allocator.free(files);
+    _ = try data_stream.read(files);
+
+    std.debug.print("[FPCK1] tree size: {any}\n", .{tree.len});
+    std.debug.print("[FPCK1] data size: {any}\n", .{files.len});
 
     std.debug.print("[FPCK1] decompressing file tree\n", .{});
 
@@ -138,6 +181,7 @@ pub fn unpack(file: std.fs.File, output: std.fs.Dir, allocator: std.mem.Allocato
         std.debug.print("[FPCK1 STG1] {any} {any} {s}\n", .{ id, path_len, path });
     }
 
+    std.debug.print("[FPCK1] decompressing file data\n", .{});
     var file_stream = std.io.fixedBufferStream(files);
     while (file_stream.pos < file_stream.buffer.len) {
         var id_buffer = [4]u8{ 0, 0, 0, 0 };
